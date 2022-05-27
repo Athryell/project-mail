07c1236725f1e881d50f68cde99994d31c440adc
Athryell <aleprovvi@yahoo.it>
2022-05-27 10:47:26 +0200

    first commit

---
+++ mail/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+
+    # API Routes
+    # path("manifest.json", views.manifest, name="manifest"),
+    path("emails", views.compose, name="compose"),
+    path("emails/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
+]
